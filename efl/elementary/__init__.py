b320b094e74fbb08585e5d4e9d58932e9a51ca18
Kai Huuhko <kai.huuhko@gmail.com>
2013-02-19 23:08:42 +0000

    python-efl: Fix generating documentation locally (without installing it). Update documentation version strings.
    SVN revision: 84244

--- efl/elementary/__init__.py
+++ efl/elementary/__init__.py
@@ -17,16 +17,16 @@
 # along with python-elementary.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from actionslider import Actionslider
-from background import Background
-from box import Box
-from bubble import Bubble
-from button import Button
-from calendar_elm import Calendar, CalendarMark
-from check import Check
-from clock import Clock
-from colorselector import Colorselector, ColorselectorPaletteItem
-from configuration import Configuration, \
+from efl.elementary.actionslider import Actionslider
+from efl.elementary.background import Background
+from efl.elementary.box import Box
+from efl.elementary.bubble import Bubble
+from efl.elementary.button import Button
+from efl.elementary.calendar_elm import Calendar, CalendarMark
+from efl.elementary.check import Check
+from efl.elementary.clock import Clock
+from efl.elementary.colorselector import Colorselector, ColorselectorPaletteItem
+from efl.elementary.configuration import Configuration, \
     config_finger_size_get, config_finger_size_set, \
     config_tooltip_delay_get, config_tooltip_delay_set, \
     focus_highlight_animate_get, focus_highlight_animate_set, \
@@ -34,80 +34,80 @@ from configuration import Configuration, \
     preferred_engine_get, preferred_engine_set, \
     engine_get, engine_set, scale_get, scale_set, \
     cursor_engine_only_get, cursor_engine_only_set
-from conformant import Conformant
-from ctxpopup import Ctxpopup, CtxpopupItem
-from datetime_elm import Datetime
-from dayselector import Dayselector
-from diskselector import Diskselector, DiskselectorItem
-from entry import Entry
-from fileselector import Fileselector
-from fileselector_button import FileselectorButton
-from fileselector_entry import FileselectorEntry
-from flip import Flip
-from flipselector import FlipSelector, FlipSelectorItem
-from frame import Frame
-from general import init, shutdown, run, exit, coords_finger_size_adjust, policy_set, policy_get
-from gengrid import Gengrid, GengridItem, GengridItemClass
-from genlist import Genlist, GenlistItem, GenlistItemClass
-from gesture_layer import GestureLayer
-from grid import Grid
-from hover import Hover
-from hoversel import Hoversel, HoverselItem
-from icon import Icon
-from image import Image
-from index import Index, IndexItem
-from innerwindow import InnerWindow
-from label import Label
-from layout import Layout
-from layout_class import LayoutClass
-from list import List, ListItem
-from map import Map, MapName, MapOverlay, MapOverlayBubble, MapOverlayCircle, MapOverlayClass, MapOverlayLine, MapOverlayPolygon, MapOverlayRoute, MapOverlayScale
-from mapbuf import Mapbuf
-from menu import Menu, MenuItem, MenuSeparatorItem
-from multibuttonentry import MultiButtonEntry, MultiButtonEntryItem
-from naviframe import Naviframe, NaviframeItem
-from need import *
-from notify import Notify
-from object import Object
-from object_item import ObjectItem
-from panel import Panel
-from panes import Panes
-from photo import Photo
-from photocam import Photocam
-from plug import Plug
-from popup import Popup, PopupItem
-from progressbar import Progressbar
-from radio import Radio
-from scroller import Scroller
-from segment_control import SegmentControl, SegmentControlItem
-from separator import Separator
-from slider import Slider
-from slideshow import Slideshow, SlideshowItem, SlideshowItemClass
-from spinner import Spinner
-from table import Table
-from theme import Theme, theme_overlay_add, theme_extension_add
-from thumb import Thumb
-from toolbar import Toolbar, ToolbarItem
-from transit import Transit
-from video import Video, Player
-from web import Web
-from window import Window, StandardWindow
+from efl.elementary.conformant import Conformant
+from efl.elementary.ctxpopup import Ctxpopup, CtxpopupItem
+from efl.elementary.datetime_elm import Datetime
+from efl.elementary.dayselector import Dayselector
+from efl.elementary.diskselector import Diskselector, DiskselectorItem
+from efl.elementary.entry import Entry
+from efl.elementary.fileselector import Fileselector
+from efl.elementary.fileselector_button import FileselectorButton
+from efl.elementary.fileselector_entry import FileselectorEntry
+from efl.elementary.flip import Flip
+from efl.elementary.flipselector import FlipSelector, FlipSelectorItem
+from efl.elementary.frame import Frame
+from efl.elementary.general import init, shutdown, run, exit, coords_finger_size_adjust, policy_set, policy_get
+from efl.elementary.gengrid import Gengrid, GengridItem, GengridItemClass
+from efl.elementary.genlist import Genlist, GenlistItem, GenlistItemClass
+from efl.elementary.gesture_layer import GestureLayer
+from efl.elementary.grid import Grid
+from efl.elementary.hover import Hover
+from efl.elementary.hoversel import Hoversel, HoverselItem
+from efl.elementary.icon import Icon
+from efl.elementary.image import Image
+from efl.elementary.index import Index, IndexItem
+from efl.elementary.innerwindow import InnerWindow
+from efl.elementary.label import Label
+from efl.elementary.layout import Layout
+from efl.elementary.layout_class import LayoutClass
+from efl.elementary.list import List, ListItem
+from efl.elementary.map import Map, MapName, MapOverlay, MapOverlayBubble, MapOverlayCircle, MapOverlayClass, MapOverlayLine, MapOverlayPolygon, MapOverlayRoute, MapOverlayScale
+from efl.elementary.mapbuf import Mapbuf
+from efl.elementary.menu import Menu, MenuItem, MenuSeparatorItem
+from efl.elementary.multibuttonentry import MultiButtonEntry, MultiButtonEntryItem
+from efl.elementary.naviframe import Naviframe, NaviframeItem
+from efl.elementary.need import *
+from efl.elementary.notify import Notify
+from efl.elementary.object import Object
+from efl.elementary.object_item import ObjectItem
+from efl.elementary.panel import Panel
+from efl.elementary.panes import Panes
+from efl.elementary.photo import Photo
+from efl.elementary.photocam import Photocam
+from efl.elementary.plug import Plug
+from efl.elementary.popup import Popup, PopupItem
+from efl.elementary.progressbar import Progressbar
+from efl.elementary.radio import Radio
+from efl.elementary.scroller import Scroller
+from efl.elementary.segment_control import SegmentControl, SegmentControlItem
+from efl.elementary.separator import Separator
+from efl.elementary.slider import Slider
+from efl.elementary.slideshow import Slideshow, SlideshowItem, SlideshowItemClass
+from efl.elementary.spinner import Spinner
+from efl.elementary.table import Table
+from efl.elementary.theme import Theme, theme_overlay_add, theme_extension_add
+from efl.elementary.thumb import Thumb
+from efl.elementary.toolbar import Toolbar, ToolbarItem
+from efl.elementary.transit import Transit
+from efl.elementary.video import Video, Player
+from efl.elementary.web import Web
+from efl.elementary.window import Window, StandardWindow
 
-from actionslider import \
+from efl.elementary.actionslider import \
     ELM_ACTIONSLIDER_NONE, \
     ELM_ACTIONSLIDER_LEFT, \
     ELM_ACTIONSLIDER_CENTER, \
     ELM_ACTIONSLIDER_RIGHT, \
     ELM_ACTIONSLIDER_ALL
 
-from background import \
+from efl.elementary.background import \
     ELM_BG_OPTION_CENTER, \
     ELM_BG_OPTION_SCALE, \
     ELM_BG_OPTION_STRETCH, \
     ELM_BG_OPTION_TILE, \
     ELM_BG_OPTION_LAST
 
-from box import \
+from efl.elementary.box import \
     ELM_BOX_LAYOUT_HORIZONTAL, \
     ELM_BOX_LAYOUT_VERTICAL, \
     ELM_BOX_LAYOUT_HOMOGENEOUS_VERTICAL, \
@@ -118,13 +118,13 @@ from box import \
     ELM_BOX_LAYOUT_FLOW_VERTICAL, \
     ELM_BOX_LAYOUT_STACK
 
-from bubble import \
+from efl.elementary.bubble import \
     ELM_BUBBLE_POS_TOP_LEFT, \
     ELM_BUBBLE_POS_TOP_RIGHT, \
     ELM_BUBBLE_POS_BOTTOM_LEFT, \
     ELM_BUBBLE_POS_BOTTOM_RIGHT
 
-from calendar_elm import \
+from efl.elementary.calendar_elm import \
     ELM_CALENDAR_UNIQUE, \
     ELM_CALENDAR_DAILY, \
     ELM_CALENDAR_WEEKLY, \
@@ -136,7 +136,7 @@ from calendar_elm import \
     ELM_CALENDAR_SELECT_MODE_NONE, \
     ELM_CALENDAR_SELECT_MODE_ONDEMAND
 
-from clock import \
+from efl.elementary.clock import \
     ELM_CLOCK_EDIT_DEFAULT, \
     ELM_CLOCK_EDIT_HOUR_DECIMAL, \
     ELM_CLOCK_EDIT_HOUR_UNIT, \
@@ -146,24 +146,24 @@ from clock import \
     ELM_CLOCK_EDIT_SEC_UNIT, \
     ELM_CLOCK_EDIT_ALL
 
-from entry import \
+from efl.elementary.entry import \
     ELM_CNP_MODE_MARKUP, \
     ELM_CNP_MODE_NO_IMAGE, \
     ELM_CNP_MODE_PLAINTEXT
 
-from colorselector import \
+from efl.elementary.colorselector import \
     ELM_COLORSELECTOR_PALETTE, \
     ELM_COLORSELECTOR_COMPONENTS, \
     ELM_COLORSELECTOR_BOTH
 
-from ctxpopup import \
+from efl.elementary.ctxpopup import \
     ELM_CTXPOPUP_DIRECTION_DOWN, \
     ELM_CTXPOPUP_DIRECTION_RIGHT, \
     ELM_CTXPOPUP_DIRECTION_LEFT, \
     ELM_CTXPOPUP_DIRECTION_UP, \
     ELM_CTXPOPUP_DIRECTION_UNKNOWN
 
-from datetime_elm import \
+from efl.elementary.datetime_elm import \
     ELM_DATETIME_YEAR, \
     ELM_DATETIME_MONTH, \
     ELM_DATETIME_DATE, \
@@ -171,7 +171,7 @@ from datetime_elm import \
     ELM_DATETIME_MINUTE, \
     ELM_DATETIME_AMPM
 
-from calendar_elm import \
+from efl.elementary.calendar_elm import \
     ELM_DAY_SUNDAY, \
     ELM_DAY_MONDAY, \
     ELM_DAY_TUESDAY, \
@@ -181,7 +181,7 @@ from calendar_elm import \
     ELM_DAY_SATURDAY, \
     ELM_DAY_LAST
 
-from dayselector import \
+from efl.elementary.dayselector import \
     ELM_DAYSELECTOR_SUN, \
     ELM_DAYSELECTOR_MON, \
     ELM_DAYSELECTOR_TUE, \
@@ -190,11 +190,11 @@ from dayselector import \
     ELM_DAYSELECTOR_FRI, \
     ELM_DAYSELECTOR_SAT
 
-from fileselector import \
+from efl.elementary.fileselector import \
     ELM_FILESELECTOR_LIST, \
     ELM_FILESELECTOR_GRID
 
-from flip import \
+from efl.elementary.flip import \
     ELM_FLIP_DIRECTION_UP, \
     ELM_FLIP_DIRECTION_DOWN, \
     ELM_FLIP_DIRECTION_LEFT, \
@@ -216,11 +216,11 @@ from flip import \
     ELM_FLIP_PAGE_UP, \
     ELM_FLIP_PAGE_DOWN
 
-from object import \
+from efl.elementary.object import \
     ELM_FOCUS_PREVIOUS, \
     ELM_FOCUS_NEXT
 
-from genlist import \
+from efl.elementary.genlist import \
     ELM_GENLIST_ITEM_NONE, \
     ELM_GENLIST_ITEM_TREE, \
     ELM_GENLIST_ITEM_GROUP, \
@@ -230,7 +230,7 @@ from genlist import \
     ELM_GENLIST_ITEM_FIELD_CONTENT, \
     ELM_GENLIST_ITEM_FIELD_STATE
 
-from gesture_layer import \
+from efl.elementary.gesture_layer import \
     ELM_GESTURE_STATE_UNDEFINED, \
     ELM_GESTURE_STATE_START, \
     ELM_GESTURE_STATE_MOVE, \
@@ -247,13 +247,13 @@ from gesture_layer import \
     ELM_GESTURE_ZOOM, \
     ELM_GESTURE_ROTATE
 
-from hover import \
+from efl.elementary.hover import \
     ELM_HOVER_AXIS_NONE, \
     ELM_HOVER_AXIS_HORIZONTAL, \
     ELM_HOVER_AXIS_VERTICAL, \
     ELM_HOVER_AXIS_BOTH
 
-from icon import \
+from efl.elementary.icon import \
     ELM_ICON_NONE, \
     ELM_ICON_FILE, \
     ELM_ICON_STANDARD
@@ -264,7 +264,7 @@ from icon import \
     #~ ELM_ILLUME_COMMAND_FOCUS_HOME, \
     #~ ELM_ILLUME_COMMAND_CLOSE
 
-from image import \
+from efl.elementary.image import \
     ELM_IMAGE_ORIENT_NONE, \
     ELM_IMAGE_ROTATE_90, \
     ELM_IMAGE_ROTATE_180, \
@@ -274,7 +274,7 @@ from image import \
     ELM_IMAGE_FLIP_TRANSPOSE, \
     ELM_IMAGE_FLIP_TRANSVERSE
 
-from entry import \
+from efl.elementary.entry import \
     ELM_INPUT_PANEL_LANG_AUTOMATIC, \
     ELM_INPUT_PANEL_LANG_ALPHABET, \
     ELM_INPUT_PANEL_LAYOUT_NORMAL, \
@@ -298,17 +298,17 @@ from entry import \
     ELM_INPUT_PANEL_RETURN_KEY_TYPE_SEARCH, \
     ELM_INPUT_PANEL_RETURN_KEY_TYPE_SEND
 
-from label import \
+from efl.elementary.label import \
     ELM_LABEL_SLIDE_MODE_NONE, \
     ELM_LABEL_SLIDE_MODE_AUTO, \
     ELM_LABEL_SLIDE_MODE_ALWAYS
 
-from list import \
+from efl.elementary.list import \
     ELM_LIST_COMPRESS, \
     ELM_LIST_SCROLL, \
     ELM_LIST_LIMIT
 
-from map import \
+from efl.elementary.map import \
     ELM_MAP_OVERLAY_TYPE_NONE, \
     ELM_MAP_OVERLAY_TYPE_DEFAULT, \
     ELM_MAP_OVERLAY_TYPE_CLASS, \
@@ -335,7 +335,7 @@ from map import \
     ELM_MAP_ZOOM_MODE_AUTO_FILL, \
     ELM_MAP_ZOOM_MODE_LAST
 
-from notify import \
+from efl.elementary.notify import \
     ELM_NOTIFY_ORIENT_TOP, \
     ELM_NOTIFY_ORIENT_CENTER, \
     ELM_NOTIFY_ORIENT_BOTTOM, \
@@ -346,31 +346,31 @@ from notify import \
     ELM_NOTIFY_ORIENT_BOTTOM_LEFT, \
     ELM_NOTIFY_ORIENT_BOTTOM_RIGHT
 
-from list import \
+from efl.elementary.list import \
     ELM_OBJECT_SELECT_MODE_DEFAULT, \
     ELM_OBJECT_SELECT_MODE_ALWAYS, \
     ELM_OBJECT_SELECT_MODE_NONE, \
     ELM_OBJECT_SELECT_MODE_DISPLAY_ONLY, \
     ELM_OBJECT_SELECT_MODE_MAX
 
-from panel import \
+from efl.elementary.panel import \
     ELM_PANEL_ORIENT_TOP, \
     ELM_PANEL_ORIENT_BOTTOM, \
     ELM_PANEL_ORIENT_LEFT, \
     ELM_PANEL_ORIENT_RIGHT
 
-from photocam import \
+from efl.elementary.photocam import \
     ELM_PHOTOCAM_ZOOM_MODE_MANUAL, \
     ELM_PHOTOCAM_ZOOM_MODE_AUTO_FIT, \
     ELM_PHOTOCAM_ZOOM_MODE_AUTO_FILL, \
     ELM_PHOTOCAM_ZOOM_MODE_AUTO_FIT_IN
 
-from general import \
+from efl.elementary.general import \
     ELM_POLICY_QUIT, \
     ELM_POLICY_QUIT_NONE, \
     ELM_POLICY_QUIT_LAST_WINDOW_CLOSED
 
-from popup import \
+from efl.elementary.popup import \
     ELM_POPUP_ORIENT_TOP, \
     ELM_POPUP_ORIENT_CENTER, \
     ELM_POPUP_ORIENT_BOTTOM, \
@@ -381,16 +381,16 @@ from popup import \
     ELM_POPUP_ORIENT_BOTTOM_LEFT, \
     ELM_POPUP_ORIENT_BOTTOM_RIGHT
 
-from scroller import \
+from efl.elementary.scroller import \
     ELM_SCROLLER_POLICY_AUTO, \
     ELM_SCROLLER_POLICY_ON, \
     ELM_SCROLLER_POLICY_OFF
 
-from entry import \
+from efl.elementary.entry import \
     ELM_TEXT_FORMAT_PLAIN_UTF8, \
     ELM_TEXT_FORMAT_MARKUP_UTF8
 
-from toolbar import \
+from efl.elementary.toolbar import \
     ELM_TOOLBAR_SHRINK_NONE, \
     ELM_TOOLBAR_SHRINK_HIDE, \
     ELM_TOOLBAR_SHRINK_SCROLL, \
@@ -398,7 +398,7 @@ from toolbar import \
     ELM_TOOLBAR_SHRINK_EXPAND, \
     ELM_TOOLBAR_SHRINK_LAST
 
-from web import \
+from efl.elementary.web import \
     ELM_WEB_WINDOW_FEATURE_TOOLBAR, \
     ELM_WEB_WINDOW_FEATURE_STATUSBAR, \
     ELM_WEB_WINDOW_FEATURE_SCROLLBARS, \
@@ -409,7 +409,7 @@ from web import \
     ELM_WEB_ZOOM_MODE_AUTO_FIT, \
     ELM_WEB_ZOOM_MODE_AUTO_FILL
 
-from window import \
+from efl.elementary.window import \
     ELM_WIN_BASIC, \
     ELM_WIN_DIALOG_BASIC, \
     ELM_WIN_DESKTOP, \
@@ -450,7 +450,7 @@ from window import \
     ELM_WIN_KEYBOARD_KEYPAD, \
     ELM_WIN_KEYBOARD_J2ME
 
-from label import \
+from efl.elementary.label import \
     ELM_WRAP_NONE, \
     ELM_WRAP_CHAR, \
     ELM_WRAP_WORD, \
