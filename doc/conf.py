224cfb92c4eb1c8c79f58d1d38476c5eeaa25e7d
davemds <dave@gurumeditation.it>
2013-04-04 21:09:39 +0200

    Python-EFL: move the sphinx themes into a proper directory

--- doc/conf.py
+++ doc/conf.py
@@ -132,7 +132,7 @@ html_theme = 'sphinxdoc' # Much modern sphinx style
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['.']
+html_theme_path = ['themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
