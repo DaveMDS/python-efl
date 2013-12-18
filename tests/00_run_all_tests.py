ea9496e2e84f54e5de1e739695341ed06f9b713f
Kai Huuhko <kai.huuhko@gmail.com>
2013-12-18 16:45:50 +0200

    Tests: Decrease verbosity and buffer the output when running full suite.

--- tests/00_run_all_tests.py
+++ tests/00_run_all_tests.py
@@ -5,5 +5,5 @@ import unittest
 
 loader = unittest.TestLoader()
 suite = loader.discover('.')
-runner = unittest.TextTestRunner(verbosity=2)
+runner = unittest.TextTestRunner(verbosity=1, buffer=True)
 result = runner.run(suite)
