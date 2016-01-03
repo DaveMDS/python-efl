0021e4a186966e887252baf923445e14ff8fac02
Dave Andreoli <dave@gurumeditation.it>
2016-01-03 18:51:38 +0100

    oh, this is really important ...   :)

--- efl/ethumb/efl.ethumb_client.pyx
+++ efl/ethumb/efl.ethumb_client.pyx
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with this Python-EFL.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 :mod:`efl.ethumb_client` Module
