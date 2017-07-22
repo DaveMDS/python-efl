741000d423deafb01c80c93eef323188234c18f0
Dave Andreoli <dave@gurumeditation.it>
2017-07-22 10:55:04 +0200

    Move elm external test from edje to elm
    elm widgets need an elm parent to work correctly, so it's better to use an elm win instead of a ecore evas one.

--- tests/elementary/test_04_external.py
+++ tests/elementary/test_04_external.py
@@ -5,43 +5,7 @@ import logging
 
 from efl import evas
 from efl import edje
-
-from efl.elementary.actionslider import Actionslider
-from efl.elementary.background import Background
-from efl.elementary.bubble import Bubble
-from efl.elementary.button import Button
-from efl.elementary.calendar_elm import Calendar
-from efl.elementary.check import Check
-from efl.elementary.clock import Clock
-from efl.elementary.entry import Entry
-from efl.elementary.fileselector import Fileselector
-from efl.elementary.fileselector_button import FileselectorButton
-from efl.elementary.fileselector_entry import FileselectorEntry
-from efl.elementary.frame import Frame
-from efl.elementary.gengrid import Gengrid
-from efl.elementary.genlist import Genlist
-from efl.elementary.hoversel import Hoversel
-from efl.elementary.icon import Icon
-from efl.elementary.index import Index
-from efl.elementary.label import Label
-from efl.elementary.list import List
-from efl.elementary.map import Map
-from efl.elementary.multibuttonentry import MultiButtonEntry
-from efl.elementary.naviframe import Naviframe
-from efl.elementary.notify import Notify
-from efl.elementary.panes import Panes
-from efl.elementary.photocam import Photocam
-from efl.elementary.progressbar import Progressbar
-from efl.elementary.radio import Radio
-from efl.elementary.scroller import Scroller
-from efl.elementary.segment_control import SegmentControl
-from efl.elementary.slider import Slider
-from efl.elementary.slideshow import Slideshow
-from efl.elementary.spinner import Spinner
-from efl.elementary.toolbar import Toolbar
-from efl.elementary.thumb import Thumb
-from efl.elementary.video import Video
-from efl.elementary.web import Web
+from efl import elementary as elm
 
 
 theme_path = os.path.dirname(os.path.abspath(__file__))
@@ -50,19 +14,18 @@ theme_file = os.path.join(theme_path, "theme.edj")
 
 class TestElementaryExternal(unittest.TestCase):
     def setUp(self):
-        self.canvas = evas.Canvas(method="buffer",
-                                  size=(400, 500),
-                                  viewport=(0, 0, 400, 500))
-        self.canvas.engine_info_set(self.canvas.engine_info_get())
-        self.theme = edje.Edje(self.canvas, file=theme_file, group="main")
+        self.win = elm.Window("t", elm.ELM_WIN_BASIC)
+        self.layout = elm.Layout(self.win, file=(theme_file, "main"))
+        self.theme = self.layout.edje
 
     def tearDown(self):
-        self.theme.delete()
-        self.canvas.delete()
+        # TODO: This is correct but throw lots of Eo errors (to be fixed in efl)
+        # self.win.delete()
+        pass
 
     def testExternalActionslider(self):
         elm_as = self.theme.part_external_object_get("ext_elm_actionslider")
-        self.assertIsInstance(elm_as, Actionslider)
+        self.assertIsInstance(elm_as, elm.Actionslider)
 
         # check values set in edc (from external)
         self.assertEqual(self.theme.part_external_param_get("ext_elm_actionslider", "label"), "ActionSlideR")
@@ -79,37 +42,37 @@ class TestElementaryExternal(unittest.TestCase):
 
     def testExternalBackground(self):
         elm_bg = self.theme.part_external_object_get("ext_elm_bg")
-        self.assertIsInstance(elm_bg, Background)
+        self.assertIsInstance(elm_bg, elm.Background)
 
         # TODO test params
 
     def testExternalBubble(self):
         elm_bg = self.theme.part_external_object_get("ext_elm_bubble")
-        self.assertIsInstance(elm_bg, Bubble)
+        self.assertIsInstance(elm_bg, elm.Bubble)
 
         # TODO test params
 
     def testExternalButton(self):
         elm_button = self.theme.part_external_object_get("ext_elm_button")
-        self.assertIsInstance(elm_button, Button)
+        self.assertIsInstance(elm_button, elm.Button)
 
         # TODO test params
 
     def testExternalCalendar(self):
         elm_cal = self.theme.part_external_object_get("ext_elm_calendar")
-        self.assertIsInstance(elm_cal, Calendar)
+        self.assertIsInstance(elm_cal, elm.Calendar)
 
         # TODO test params
 
     def testExternalCheck(self):
         elm_check = self.theme.part_external_object_get("ext_elm_check")
-        self.assertIsInstance(elm_check, Check)
+        self.assertIsInstance(elm_check, elm.Check)
 
         # TODO test params
 
     def testExternalClock(self):
         elm_clock = self.theme.part_external_object_get("ext_elm_clock")
-        self.assertIsInstance(elm_clock, Clock)
+        self.assertIsInstance(elm_clock, elm.Clock)
 
         # check values set in edc (from external)
         self.assertEqual(self.theme.part_external_param_get("ext_elm_clock", "hours"), 23)
@@ -141,67 +104,67 @@ class TestElementaryExternal(unittest.TestCase):
 
     def testExternalEntry(self):
         elm_entry = self.theme.part_external_object_get("ext_elm_entry")
-        self.assertIsInstance(elm_entry, Entry)
+        self.assertIsInstance(elm_entry, elm.Entry)
 
         # TODO test params
 
     def testExternalFileselector(self):
         elm_fs = self.theme.part_external_object_get("ext_elm_fileselector")
-        self.assertIsInstance(elm_fs, Fileselector)
+        self.assertIsInstance(elm_fs, elm.Fileselector)
 
         # TODO test params
 
     def testExternalFileselectorButton(self):
         elm_fs_btn = self.theme.part_external_object_get("ext_elm_fileselector_button")
-        self.assertIsInstance(elm_fs_btn, FileselectorButton)
+        self.assertIsInstance(elm_fs_btn, elm.FileselectorButton)
 
         # TODO test params
 
     def testExternalFileselectorEntry(self):
         elm_fs_en = self.theme.part_external_object_get("ext_elm_fileselector_entry")
-        self.assertIsInstance(elm_fs_en, FileselectorEntry)
+        self.assertIsInstance(elm_fs_en, elm.FileselectorEntry)
 
         # TODO test params
 
     def testExternalFrame(self):
         elm_frame = self.theme.part_external_object_get("ext_elm_frame")
-        self.assertIsInstance(elm_frame, Frame)
+        self.assertIsInstance(elm_frame, elm.Frame)
 
         # TODO test params
 
     def testExternalGengrid(self):
         elm_gg = self.theme.part_external_object_get("ext_elm_gengrid")
-        self.assertIsInstance(elm_gg, Gengrid)
+        self.assertIsInstance(elm_gg, elm.Gengrid)
 
         # TODO test params
 
     def testExternalGenlist(self):
         elm_gl = self.theme.part_external_object_get("ext_elm_genlist")
-        self.assertIsInstance(elm_gl, Genlist)
+        self.assertIsInstance(elm_gl, elm.Genlist)
 
         # TODO test params
 
     def testExternalHoversel(self):
         elm_hoversel = self.theme.part_external_object_get("ext_elm_hoversel")
-        self.assertIsInstance(elm_hoversel, Hoversel)
+        self.assertIsInstance(elm_hoversel, elm.Hoversel)
 
         # TODO test params
 
     def testExternalIcon(self):
         elm_icon = self.theme.part_external_object_get("ext_elm_icon")
-        self.assertIsInstance(elm_icon, Icon)
+        self.assertIsInstance(elm_icon, elm.Icon)
 
         # TODO test params
 
     def testExternalIndex(self):
         elm_index = self.theme.part_external_object_get("ext_elm_index")
-        self.assertIsInstance(elm_index, Index)
+        self.assertIsInstance(elm_index, elm.Index)
 
         # TODO test params
 
     def testExternalLabel(self):
         elm_label = self.theme.part_external_object_get("ext_elm_label")
-        self.assertIsInstance(elm_label, Label)
+        self.assertIsInstance(elm_label, elm.Label)
 
         # check values setted in edc (from external)
         self.assertEqual(self.theme.part_external_param_get("ext_elm_label", "label"), "This is an elm label")
@@ -218,73 +181,73 @@ class TestElementaryExternal(unittest.TestCase):
 
     def testExternalList(self):
         elm_list = self.theme.part_external_object_get("ext_elm_list")
-        self.assertIsInstance(elm_list, List)
+        self.assertIsInstance(elm_list, elm.List)
 
         # TODO test params
 
     def testExternalMap(self):
         elm_map = self.theme.part_external_object_get("ext_elm_map")
-        self.assertIsInstance(elm_map, Map)
+        self.assertIsInstance(elm_map, elm.Map)
 
         # TODO test params
 
     def testExternalMultibuttonentry(self):
         elm_mbe = self.theme.part_external_object_get("ext_elm_multibuttonentry")
-        self.assertIsInstance(elm_mbe, MultiButtonEntry)
+        self.assertIsInstance(elm_mbe, elm.MultiButtonEntry)
 
         # TODO test params
 
     def testExternalNaviframe(self):
         elm_nf = self.theme.part_external_object_get("ext_elm_naviframe")
-        self.assertIsInstance(elm_nf, Naviframe)
+        self.assertIsInstance(elm_nf, elm.Naviframe)
 
         # TODO test params
 
     def testExternalNotify(self):
         elm_notify = self.theme.part_external_object_get("ext_elm_notify")
-        self.assertIsInstance(elm_notify, Notify)
+        self.assertIsInstance(elm_notify, elm.Notify)
 
         # TODO test params
 
     def testExternalPanes(self):
         elm_panes = self.theme.part_external_object_get("ext_elm_panes")
-        self.assertIsInstance(elm_panes, Panes)
+        self.assertIsInstance(elm_panes, elm.Panes)
 
         # TODO test params
 
     def testExternalPhotocam(self):
         elm_photocam = self.theme.part_external_object_get("ext_elm_photocam")
-        self.assertIsInstance(elm_photocam, Photocam)
+        self.assertIsInstance(elm_photocam, elm.Photocam)
 
         # TODO test params
 
     def testExternalProgressbar(self):
         elm_pb = self.theme.part_external_object_get("ext_elm_progressbar")
-        self.assertIsInstance(elm_pb, Progressbar)
+        self.assertIsInstance(elm_pb, elm.Progressbar)
 
         # TODO test params
 
     def testExternalRadio(self):
         elm_radio = self.theme.part_external_object_get("ext_elm_radio")
-        self.assertIsInstance(elm_radio, Radio)
+        self.assertIsInstance(elm_radio, elm.Radio)
 
         # TODO test params
 
     def testExternalScroller(self):
         elm_scroller = self.theme.part_external_object_get("ext_elm_scroller")
-        self.assertIsInstance(elm_scroller, Scroller)
+        self.assertIsInstance(elm_scroller, elm.Scroller)
 
         # TODO test params
 
     def testExternalSegmentControl(self):
         elm_sc = self.theme.part_external_object_get("ext_elm_segment_control")
-        self.assertIsInstance(elm_sc, SegmentControl)
+        self.assertIsInstance(elm_sc, elm.SegmentControl)
 
         # TODO test params
 
     def testExternalSlider(self):
         elm_slider = self.theme.part_external_object_get("ext_elm_slider")
-        self.assertIsInstance(elm_slider, Slider)
+        self.assertIsInstance(elm_slider, elm.Slider)
 
         # check values setted in edc (from external)
         self.assertEqual(self.theme.part_external_param_get("ext_elm_slider", "label"), "external slider")
@@ -306,7 +269,7 @@ class TestElementaryExternal(unittest.TestCase):
         self.assertEqual(elm_slider.span_size, 45)
         self.assertEqual(elm_slider.unit_format, "test %f")
         self.assertEqual(elm_slider.indicator_format, "%f ind")
-        self.assertIsInstance(elm_slider.icon, Icon)
+        self.assertIsInstance(elm_slider.icon, elm.Icon)
 
         # do params set
         self.theme.part_external_param_set("ext_elm_slider", "label", "new text")
@@ -329,42 +292,42 @@ class TestElementaryExternal(unittest.TestCase):
         self.assertEqual(elm_slider.span_size, 11)
         self.assertEqual(elm_slider.unit_format, "uf")
         self.assertEqual(elm_slider.indicator_format, "if")
-        self.assertIsInstance(elm_slider.icon, Icon)
+        self.assertIsInstance(elm_slider.icon, elm.Icon)
 
     def testExternalSlideshow(self):
         elm_ss = self.theme.part_external_object_get("ext_elm_slideshow")
-        self.assertIsInstance(elm_ss, Slideshow)
+        self.assertIsInstance(elm_ss, elm.Slideshow)
 
         # TODO test params
 
     def testExternalSpinner(self):
         elm_spinner = self.theme.part_external_object_get("ext_elm_spinner")
-        self.assertIsInstance(elm_spinner, Spinner)
+        self.assertIsInstance(elm_spinner, elm.Spinner)
 
         # TODO test params
 
     def testExternalThumb(self):
         elm_thumb = self.theme.part_external_object_get("ext_elm_thumb")
-        self.assertIsInstance(elm_thumb, Thumb)
+        self.assertIsInstance(elm_thumb, elm.Thumb)
 
         # TODO test params
 
     def testExternalToolbar(self):
         elm_toolbar = self.theme.part_external_object_get("ext_elm_toolbar")
-        self.assertIsInstance(elm_toolbar, Toolbar)
+        self.assertIsInstance(elm_toolbar, elm.Toolbar)
 
         # TODO test params
 
     def testExternalVideo(self):
         elm_video = self.theme.part_external_object_get("ext_elm_video")
-        self.assertIsInstance(elm_video, Video)
+        self.assertIsInstance(elm_video, elm.Video)
 
         # TODO test params
 
     @unittest.skip("Elm Web not maintained atm")
     def testExternalWeb(self):
         elm_web = self.theme.part_external_object_get("ext_elm_web")
-        self.assertIsInstance(elm_web, Web)
+        self.assertIsInstance(elm_web, elm.Web)
 
         # TODO test params
 
